92734a6e37f22e489470177c726d553b7da32524
Илья Матвеев <laskovoebo@gmail.com>
2021-06-17 20:23:18 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style/App.css';
+import HeaderTable from './HeaderTable';
+
+function App() {
+  return (
+    <HeaderTable />
+  );
+}
+
+export default App;
---
+++ src/HeaderTable/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './headerTable.css';
+
+const HeaderTable = () => {
+  console.log('render'); // это просто чтобы был return
+  return (
+    <div className="table" />
+  );
+};
+
+export default HeaderTable;
