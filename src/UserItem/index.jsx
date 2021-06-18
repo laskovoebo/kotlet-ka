8779212e7fc68f0c99c65b94d2049360a489fd73
Max Beketov <mbeketov18@gmail.com>
2021-06-18 16:43:03 +0300

    handle delete

--- src/UserItem/index.jsx
+++ src/UserItem/index.jsx
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './userItem.css';
 
-const UserItem = ({ item: { id, name, count } }) => (
-  <div className="headerTable">
-    <div className="userItemId">
-      {id}
-    </div>
-    <div className="userItemName">
-      {name}
-    </div>
-    <div className="userItemCount">
-      {count}
-    </div>
-    <div className="userDelete">
-      <button type="button">
-        delete
-      </button>
-    </div>
-  </div>
-);
+class UserItem extends Component {
+  handleDelete = () => {
+    const { item: { id } } = this.props;
+    console.log(id);
+  }
+
+  render() {
+    const { item: { id, name, count } } = this.props;
+    return (
+      <div className="headerTable">
+        <div className="userItemId">
+          {id}
+        </div>
+        <div className="userItemName">
+          {name}
+        </div>
+        <div className="userItemCount">
+          {count}
+        </div>
+        <div className="userDelete">
+          <button type="button" onClick={this.handleDelete}>
+            delete
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 UserItem.propTypes = {
   item: PropTypes.shape({
